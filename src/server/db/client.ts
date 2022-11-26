1a3578045bb1e29d091fd020ccea7779e5a87f42
Kian Saeidnia <ksaeidnia06@gmail.com>
2022-11-26 12:49:32 +0100

    integrated supabase instead of planetscale as db

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,6 +1,7 @@
 // src/server/db/client.ts
 import { PrismaClient } from "@prisma/client";
 import { env } from "../../env/server.mjs";
+import { createClient } from '@supabase/supabase-js'
 
 declare global {
   // eslint-disable-next-line no-var
@@ -17,3 +18,33 @@ export const prisma =
 if (env.NODE_ENV !== "production") {
   global.prisma = prisma;
 }
+
+
+const options = {
+  db: {
+
+    schema: 'public',
+
+  },
+
+  auth: {
+
+    autoRefreshToken: true,
+
+    persistSession: true,
+
+    detectSessionInUrl: true
+
+  }
+}
+
+const supabaseUrl = 'https://rmtxrknumvkqbufnysqp.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+let supabase;
+if (supabaseKey && supabaseUrl) {
+  supabase = createClient(supabaseUrl, supabaseKey, options)
+} else {
+  console.log("Please make sure a supabase URL and Key are set.")
+}
+
+export {supabase};
